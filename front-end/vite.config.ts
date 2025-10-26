4fc7956f52c466085dc032b5bae0262736e896d2
ychleo102615 <leo102615@gmail.com>
2025-10-26 13:24:02 +0800

    feat: 加入 svg icon

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -4,11 +4,41 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import path from 'path'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    createSvgIconsPlugin({
+      // 指定需要快取的圖示資料夾路徑
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+
+      // 指定 symbolId 格式 - 用於花牌專案的結構化命名
+      symbolId: 'icon-[dir]-[name]',
+
+      // SVG 優化選項
+      svgoOptions: {
+        plugins: [
+          {
+            name: 'preset-default',
+            params: {
+              overrides: {
+                removeViewBox: false, // 保留 viewBox 以確保縮放正確
+              },
+            },
+          },
+        ],
+      },
+
+      // DOM 插入位置
+      inject: 'body-last',
+
+      // 自定義容器 ID
+      customDomId: '__playing_cards_svg_sprite__',
+    }),
   ],
   resolve: {
     alias: {
